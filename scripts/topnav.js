f4495d60cc1583b57856ded06ad6dda8e55ead37
Terence Parr <parrt@antlr.org>
2013-01-05 15:22:10 -0800

    forgot to add

---
+++ scripts/topnav.js
@@ -0,0 +1,7 @@
+document.write("<ul>");
+document.write("<li><a href='download.html'>Download<span>v4.0.6</span></a></li>\n");
+document.write("<li><a href='http://www.antlr.org/wiki/display/ST4/StringTemplate+4+Documentation'>Doc</a></li>\n");
+document.write("<li><a href='about.html'>About ST</a></li>\n");
+document.write("<li><a href='support.html'>Support</a></li>  \n");
+document.write("<li><a href='https://github.com/antlr/stringtemplate4/issues'>Bugs</a></li> \n");
+document.write("</ul>\n");
