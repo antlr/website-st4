8d7f9291283accfa0793509f48dad619a7e5e335
Terence Parr <parrt@antlr.org>
2014-03-25 09:12:48 -0700

    update Download link in navigation bar

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,5 +1,5 @@
 document.write("<ul>");
-document.write("<li><a href='download.html'>Download<span>v4.0.7</span></a></li>\n");
+document.write("<li><a href='download.html'>Download<span>v4.0.8</span></a></li>\n");
 document.write("<li><a href='https://theantlrguy.atlassian.net/wiki/display/ST4/StringTemplate+4+Documentation'>Doc</a></li>\n");
 document.write("<li><a href='about.html'>About ST</a></li>\n");
 document.write("<li><a href='support.html'>Support</a></li>  \n");
