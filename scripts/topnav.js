6805ef50a36cff6b65943e2d9b5e3c65337fb4f9
Terence Parr <parrt@antlr.org>
2015-12-09 10:42:13 -0800

    point at new doc

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,6 +1,6 @@
 document.write("<ul>");
 document.write("<li><a href='download.html'>Download<span>v4.0.8</span></a></li>\n");
-document.write("<li><a href='https://theantlrguy.atlassian.net/wiki/display/ST4/StringTemplate+4+Documentation'>Doc</a></li>\n");
+document.write("<li><a href='https://raw.githubusercontent.com/antlr/stringtemplate4/master/doc/index.md'>Doc</a></li>\n");
 document.write("<li><a href='about.html'>About ST</a></li>\n");
 document.write("<li><a href='support.html'>Support</a></li>  \n");
 document.write("<li><a href='https://github.com/antlr/stringtemplate4/issues'>Bugs</a></li> \n");
