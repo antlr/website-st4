ca21f21adf5728c7d49745dfbf0e1fb11d9ff67c
Terence Parr <parrt@antlr.org>
2022-04-02 11:14:05 -0700

    push 4.3.2

---
+++ api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.stringtemplate.v4"},{"l":"org.stringtemplate.v4.compiler"},{"l":"org.stringtemplate.v4.debug"},{"l":"org.stringtemplate.v4.gui"},{"l":"org.stringtemplate.v4.misc"}]
